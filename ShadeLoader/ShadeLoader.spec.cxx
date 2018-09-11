8aa848781a31a0fc1c12f0e194951dea0db68708
Taylor Kaplan <taylor@pocketapp.io>
2018-09-10 20:52:49 -0600

    Save

---
+++ ShadeLoader/ShadeLoader.spec.cxx
@@ -0,0 +1,45 @@
+#include "gtest/gtest.h"
+
+namespace {
+
+// The fixture for testing class Foo.
+class ShadeLoaderTest : public ::testing::Test {
+ protected:
+  // You can remove any or all of the following functions if its body
+  // is empty.
+
+  ShadeLoaderTest() {
+     // You can do set-up work for each test here.
+  }
+
+  ~ShadeLoaderTest() override {
+     // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  void SetUp() override {
+     // Code here will be called immediately after the constructor (right
+     // before each test).
+  }
+
+  void TearDown() override {
+     // Code here will be called immediately after each test (right
+     // before the destructor).
+  }
+
+  // Objects declared here can be used by all tests in the test case for Foo.
+};
+
+// Tests that the Foo::Bar() method does Abc.
+TEST_F(ShadeLoaderTest, MethodBarDoesAbc) {
+
+}
+
+}  // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
